953650f01fc0e9038193e92dbcedf8d385ad6d0f
Ileana <ileanam@uoregon.edu>
2024-06-07 18:13:15 -0700

    Create Lab 2-node.js

---
+++ Lab 2-node.js
@@ -0,0 +1,11 @@
+/* 
+    CIT 281 Lab 2 
+    Name: Ileana Matthews
+*/
+function square(num) {
+    return num*num;
+}
+console.log('square operations:')
+for (let i = 2;i <= 10; i+=2) {
+    console.log(`Square of 20 is ${square(20)}`);
+}
